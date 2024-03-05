9f4de86d2c344ba6e531acb7fa7239fcdcf8b92f
CipiVlad <102032121+CipiVlad@users.noreply.github.com>
2024-03-05 01:34:55 +0100

    refactored main.ts, renderTicket function with map

---
+++ src/createTicket.ts
@@ -0,0 +1,8 @@
+
+// function createTicket() {
+//     console.log('ticket created!')
+//     console.log(store);
+
+// }
+
+// export default createTicket;
--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,104 @@
 // Overview Logic
 import { v4 as uuid } from 'uuid';
-const ticketId = uuid();
+
 
 // ticket type objects
-type ProblemOptions = {
-    hardware: boolean,
-    software: boolean,
+export type ProblemOptions = {
+    hardware: "yes" | "no",
+    software: "yes" | "no",
     solvingStatus: "done" | "undone"
     priorityStatus: "low" | "mid" | "high"
 }
 
-type ProblemDescription = {
+export type ProblemDescription = {
     id: string,
     title: string,
     description: string,
 }
 
+export type Ticket = ProblemOptions & ProblemDescription;
 
-type Ticket = ProblemOptions & ProblemDescription;
 
-//example
+// Ticket Array Store
+export let store: Ticket[] = [];
+
+//create new ticket function
+function createNewTicket(props: Ticket) {
+    const thisTicket = {
+        id: uuid(),
+        hardware: props.hardware,
+        software: props.software,
+        solvingStatus: props.solvingStatus,
+        priorityStatus: props.priorityStatus,
+        title: props.title,
+        description: props.description
+    }
+
+    return store.push(thisTicket)
+}
+
+
+//examples tickets hard coded 
 
 const newTicket: Ticket = {
-    id: ticketId,
-    hardware: false,
-    software: true,
+    id: uuid(),
+    hardware: "no",
+    software: "yes",
     solvingStatus: "undone",
     priorityStatus: "high",
     title: "ERROR 404",
     description: "Page Not Found"
 }
 
-
-// render ticket
-const renderTicket = () => {
-    // for (const [key, value] of Object.entries(newTicket)) {
-    //     return ticketNumber.innerHTML = (
-    //         `${key}: ${value}`
-    //     )
-    // }
-    return (
-        document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-
-<header>
-<nav class="navBar">
-  <h1>Ticketing Help Desk Service</h1>
-  <a href="index.html">Overview</a>
-  <a href="problemInput.html">Create Ticket</a>
-</nav>
-</header>
+createNewTicket(newTicket)
 
 
-<main>
-<h2>Ticket Overview</h2>
-<!-- here goes dynamic rendering -->
-<section id="ticketCard" class="problems_container software">
+//create ticket via generator function
 
-  <h4 id="title">${newTicket.title}</h4>
-  <p id="ticketNumber">Ticket Number: ${newTicket.id}</p>
-  <p id="description">${newTicket.description}</p>
-  
-  <p id="hardware">Hardware: ${newTicket.hardware}</p>
-  <p id="software">Software:${newTicket.software}</p>
-  <p id="status">Progress: ${newTicket.solvingStatus}</p>
-  <p id="priority">Priority: ${newTicket.priorityStatus}</p>
-</section>
-<section class="problems_container hardware"></section>
-</main>`
-    )
+createNewTicket({
+    id: uuid(),
+    hardware: "no",
+    software: "yes",
+    solvingStatus: "undone",
+    priorityStatus: "low",
+    title: "shu",
+    description: "login not working"
 
-}
+});
 
-renderTicket()
+createNewTicket({
+    id: uuid(),
+    hardware: "yes",
+    software: "yes",
+    solvingStatus: "done",
+    priorityStatus: "high",
+    title: "shu -works again",
+    description: "login works"
+});
+
+console.log(store);
+
+
+// render card
+
+const renderTicket = store.map((ticket, index) => {
+    return ` 
+    <main class="renderTicketCardContainer">
+            <section key=${index} id="ticketCard" class="problems_container">    
+                <h4 id="title">Incidend: <a href="./detail.html">${ticket.title}</a></h4>
+                <p id="ticketNumber">Ticket Number: ${ticket.id}</p>
+                <p id="description">Description: ${ticket.description}</p>
+                <p id="hardware">Hardware: ${ticket.hardware}</p>
+                <p id="software">Software: ${ticket.software}</p>
+                <p id="status">Progress: ${ticket.solvingStatus}</p>
+                <p id="priority">Priority: ${ticket.priorityStatus}</p>
+            </section>
+    </main>`
+
+});
+
+
+
+document.querySelector<HTMLDivElement[]>('#app')!.innerHTML = renderTicket;
+
 
